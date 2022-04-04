23cd57af7a8784d331cb88c166718fbef0edfe04
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-04 15:24:04 -0500

    hover events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
 
-
 function App() {
 
+    const [headingText, setHeadingText] = useState("Hello");
+    const [isMouseOver, setIsMouseOver] = useState(false)
 
-    const now = new Date().toLocaleTimeString();
-    const [time, setTime] = useState(now)
-    
 
-    function clicked() {
-    const timeNow = new Date().toLocaleTimeString();
-        setTime(timeNow);
+    function handleClick() {
+        setHeadingText("Submitted");
+    }
+    function changeBackground(){
+        setIsMouseOver(true);
+    }
+    function changeBackgroundAgain(){
+        setIsMouseOver(false);
     }
 
-
-    return (
-        <div className="container">
-            <h1>{time}</h1>
-            <button onClick={clicked}>Get Time Austin!</button>
-        </div>
-    )
+return (
+<div className="container">
+    <h1>{headingText}</h1>
+    <input type="text" placeholder="What's your name?" />
+    <button style={{ backgroundColor: isMouseOver ? "yellow" : "white"}} 
+    onClick={handleClick} 
+    onMouseOver={changeBackground} 
+    onMouseOut={changeBackgroundAgain}>
+    Submit</button>
+</div>
+);
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./components/App";
-import cars from "./practice"
+import App from "./components/App";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <table>
-        <tr>
-            <th>Brand</th>
-            <th>Top Speed</th>
-        </tr>
-        <tr>
-            <td>{cars[1].model}</td>
-            <td>{cars[1].speedStats.topSpeed}</td>
-            <td>{cars[1].coloursByPopularity}</td>
-        </tr>
-        <tr>
-            <td>{cars[0].model}</td>
-            <td>{cars[0].speedStats.topSpeed}</td>
-            <td>{cars[0].coloursByPopularity}</td>
-        </tr>
-    </table>
+<App />
 )
 
