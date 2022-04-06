a23ceff36eb10f8e21f1e17912f498468fd87a19
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-06 15:23:20 -0500

    Home Page Update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,62 @@
 import Logo from '../../assets/images/Koray-img.jpeg'
 import { Link } from 'react-router-dom'
 import './index.scss'
-
-
+import AnimatedLetters from '../AnimatedLetters';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const nameArray = ['','','K', 'o', 'r', 'a', 'y','']
+    const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+]
+useEffect(() => {
+    return setTimeout(() => {
+        setLetterClass('text-animate-hover')
+    }, 4000)
+}, [])
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                    Koray
+                    <h1><span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
                     <br />
-                    Web Developer
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                    I'm a
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
                     <br />
                 </h1>
-                <h2>Frontend Developer</h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
             </div>
-            <div>
+            <div className="self-img-div">
             <img src={Logo} alt="developer" className="self-img"/>
             </div>
         </div>
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,62 +1,61 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
-import KLogo from '../../assets/images/Koray.gif'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope, faCode } from '@fortawesome/free-solid-svg-icons'
 import {
     faLinkedin,
     faGithub,
 } from '@fortawesome/free-brands-svg-icons'
 
+
 const Sidebar = () => {
     return (
         <div className="nav-bar">
-        <Link className="logo" to="/">
-            <img src={KLogo} alt="Logo" />
-            
-        </Link>
-        <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        
-        <NavLink activeclassname="active" className="about-link" to="/about">
-            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        
-        <NavLink
-        activeclassname="active"
-        className="contact-link"
-        to="/contact">
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-        </nav>
-        <ul>
-        <li>
-            <a
-            href="https://www.linkedin.com/in/koray-kurtoglu/"
-            target="_blank"
-            rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-            </a>
-        </li>
+                <a className="logo" href="https://drive.google.com/file/d/1u_mE9EFCArAh_hTZeUjJt5UYFhfpjPFD/view?usp=sharing" download="Koray-Resume.pdf" target="_blank" rel="noReferrer"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDv6iqDOwYY9_i4IT_ZvrFdhPXN2oGQhcLfg&usqp=CAU" id="social-logo" alt='Koray-Resume' />
+                </a>    
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                
+                <NavLink activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                
+                <NavLink
+                    activeclassname="active"
+                    className="contact-link"
+                    to="/contact">
+                <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+            <li>
+                <a
+                href="https://www.linkedin.com/in/koray-kurtoglu/"
+                target="_blank"
+                rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
 
-        <li>
-            <a
-            href="https://github.com/koray7"
-            target="_blank"
-            rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-            </a>
-        </li>
+            <li>
+                <a
+                href="https://github.com/koray7"
+                target="_blank"
+                rel="noreferrer">
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
 
-        <li>
-            <a href="mailto:softwarekoray@gmail.com" rel="noreferrer" target="_blank">
-            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </a>
-        </li>
+            <li>
+                <a href="mailto:softwarekoray@gmail.com" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </a>
+            </li>
 
-        </ul>
+            </ul>
         </div>
     )
 }
