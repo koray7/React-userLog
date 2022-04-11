d344ab303629359855106a650a1b762674e3a5d7
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-11 11:00:48 -0500

    Travel List Part1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-    const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-    });
-
-    function handleChange(event) {
-        const { name, value } = event.target
-
-        setContact(prevValue => {
-            return {
-                ...prevValue,
-                [name]: value
-            };
-        });
-    }
-
     return (
     <div className="container">
-        <h1>
-        Hello {contact.fName} {contact.lName}
-        </h1>
-        <p>{contact.email}</p>
-        <form>
-        <input onChange={handleChange} name="fName" placeholder="First Name" />
-        <input onChange={handleChange} name="lName" placeholder="Last Name" />
-        <input onChange={handleChange} name="email" placeholder="Email" />
-        <button>Submit</button>
-        </form>
+        <div className="heading">
+            <h1>To-Do List</h1>
+        </div>
+        <div className="form">
+            <input type="text" />
+            <button>
+                <span>Add</span>
+            </button>
+        </div>
+        <div>
+            <ul>
+                <li>A Item </li>
+            </ul>
+        </div>
     </div>
     );
 }
